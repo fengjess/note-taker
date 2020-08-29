c43166ea5f4a31734396025b2884cefeadb7ba8e
Jessica Feng <jessicafeng14@yahoo.com>
2020-08-28 22:31:37 -0700

    created routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const dbNotes = require('../db/db.json');
+
+module.exports = function (app) {
+
+    app.get('/api/notes', function (req, res) {
+        fs.readFile('./db/db.json', 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            notes = JSON.parse(data);
+            res.json(notes);
+        });
+    });
+
+    app.post('/api/notes', function (req, res) {
+
+        let notes = req.body;
+        notes.id = uuidv4();
+
+        let notesArr = [];
+
+        fs.readFile('./db/db.json', 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            notesArr = JSON.parse(data);
+            notesArr.push(notes);
+
+            fs.writeFile('./db/db.json', JSON.stringify(notesArr), function (err) {
+                if (err) {
+                    console.log(err);
+                }
+                return res.json(notesArr);
+            });
+        });
+
+    });
+
+    app.delete('/api/notes/:id', function (req, res) {
+        let noteId = req.params.id;
+        fs.readFile('./db/db.json', 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            const notesArr = JSON.parse(data);
+
+            console.log(notesArr)
+
+            const newNotesArr = notesArr.filter(item => item.id !== noteId);
+
+            console.log("-------------")
+
+            console.log(newNotesArr)
+
+            fs.writeFile('./db/db.json', JSON.stringify(newNotesArr), function (err) {
+                if (err) {
+                    console.log(err)
+                }
+                return res.json(dbNotes);
+            })
+
+        });
+    });
+
+}
